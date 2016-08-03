2e8fa4a26f44f88175ede950c35c365cd3fefbe9
Juan Luis Garcia <jlgarciaap@gmail.com>
2016-08-03 16:58:33 +0200

    Proyecto terminado

---
+++ js/tributeJS.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#title1").css("color","red");
+    $("h2").css("color","blue");
+    $("#img-mario").css("width","200px");
+    $("#cronoImage").css("width","500px");
+
+
+});
